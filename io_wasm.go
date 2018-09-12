44e3bb3d594d10a1a9726bc0e2631e0d86e43d12
Dmitri Shuralyov <dmitri@shuralyov.com>
2018-09-12 05:06:27 -0400

    WIP: Begin WebAssembly support.

--- io_wasm.go
+++ io_wasm.go
@@ -1,46 +1,43 @@
-// +build js,!wasm
+// +build js,wasm
 
 package main
 
 import (
 	"io"
-
-	"honnef.co/go/js/dom"
+	"syscall/js"
 )
 
-var document = dom.GetWindow().Document()
+var document = js.Global().Get("document")
 
 func init() {
 	// The default os.Stdout, os.Stderr are printed to browser's console, which isn't a friendly interface.
 	// Create an implementation of stdin, stdout, stderr that use a <input> and <pre> html elements.
-	stdin = NewReader(document.GetElementByID("input").(*dom.HTMLInputElement))
-	stdout = NewWriter(document.GetElementByID("output").(*dom.HTMLPreElement))
-	stderr = NewWriter(document.GetElementByID("output").(*dom.HTMLPreElement))
+	stdin = NewReader(document.Call("getElementById", "input"))
+	stdout = NewWriter(document.Call("getElementById", "output"))
+	stderr = NewWriter(document.Call("getElementById", "output"))
 
 	// Send a copy of stdin to stdout (like in most terminals).
 	stdin = io.TeeReader(stdin, stdout)
 
 	// When console is clicked, focus the input element.
 	// TODO: Make it possible/friendlier to copy the text from stdout...
-	document.GetElementByID("console").AddEventListener("click", false, func(event dom.Event) {
-		document.GetElementByID("input").(dom.HTMLElement).Focus()
-		event.PreventDefault()
-	})
+	document.Call("getElementById", "console").Call("addEventListener", "click", js.NewEventCallback(js.PreventDefault, func(js.Value) {
+		document.Call("getElementById", "input").Call("focus")
+	}))
 }
 
 // NewReader takes an <input> element and makes an io.Reader out of it.
-func NewReader(input *dom.HTMLInputElement) io.Reader {
+func NewReader(input js.Value) io.Reader {
 	r := &reader{
 		in: make(chan []byte, 8),
 	}
-	input.AddEventListener("keydown", false, func(event dom.Event) {
-		ke := event.(*dom.KeyboardEvent)
-		if ke.KeyCode == '\r' {
-			r.in <- []byte(input.Value + "\n")
-			input.Value = ""
-			ke.PreventDefault()
+	input.Call("addEventListener", "keydown", js.NewEventCallback(0, func(ke js.Value) {
+		if ke.Get("keyCode").Int() == '\r' {
+			r.in <- []byte(input.Get("value").String() + "\n")
+			input.Set("value", "")
+			// TODO: Can't call preventDefault here; what to do? See issue https://golang.org/issue/26045.
 		}
-	})
+	}))
 	return r
 }
 
@@ -59,15 +56,15 @@ func (r *reader) Read(p []byte) (n int, err error) {
 }
 
 // NewWriter takes a <pre> element and makes an io.Writer out of it.
-func NewWriter(pre *dom.HTMLPreElement) io.Writer {
+func NewWriter(pre js.Value) io.Writer {
 	return &writer{pre: pre}
 }
 
 type writer struct {
-	pre *dom.HTMLPreElement
+	pre js.Value
 }
 
 func (w *writer) Write(p []byte) (n int, err error) {
-	w.pre.SetTextContent(w.pre.TextContent() + string(p))
+	w.pre.Set("textContent", w.pre.Get("textContent").String()+string(p))
 	return len(p), nil
 }
